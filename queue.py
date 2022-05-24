d5cc9711090dcfd868a11bc483598147abf8842b
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-23 23:17:17 -0300

    Queue, tail, head

--- queue.py
+++ queue.py
@@ -1,7 +1,22 @@
 class Queue:
     def __init__(self):
-        self.queue = []
-        self.head = None
-        self.tail = None
+        self.__queue = []
+        self.__head = None
+        self.__tail = None
     
-    
+    @property
+    def queue(self):
+        return self.__queue
+
+    @property
+    def head(self):
+        return self.__head
+
+    @property
+    def tail(self):
+        return self.__tail
+
+q1=Queue()
+print(q1.head)
+print(q1.tail)
+print(q1.queue)
