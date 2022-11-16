cd94c5c7e9e7e58c6528a70ab594bd2768951b0e
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-11-16 10:34:40 -0300

    Atualizado get all para retornar dict

--- vector_unsorted.py
+++ vector_unsorted.py
@@ -1,49 +1,52 @@
 from typing import List, Union, Dict
 
+
 class VectorUnsorted:
     def __init__(self, size) -> None:
         self.__size = size
         self.__pointer = -1
         self.__vector = []
-    
-    def __empty(self)->str:
+
+    def __empty(self) -> str:
         return f"Lista vazia"
-    
+
     def length(self):
         return len(self.__vector)
 
     # GET all / item
-    def get_all(self)-> Union[List[int], str]:
+    def get_all(self) -> Union[Dict[int, int], str]:
         if self.__pointer == -1:
             return self.__empty()
-        return self.__vector
-    
+        dic = {}
+        for i in range(self.length()):
+            dic.update({i: self.__vector[i]})
+        return dic
+
     def get_item(self, item: int) -> Union[Dict[str, int], str]:
         if self.__pointer == -1:
             return self.__empty()
         else:
-            for ind,_ in enumerate(self.__vector):
+            for ind, _ in enumerate(self.__vector):
                 if self.__vector[ind] == item:
-                    return {ind:self.__vector[ind]}
-            
-            return f"Item {item} não existe na lista"
-        
+                    return {ind: self.__vector[ind]}
 
-    def insert_item(self, item: int)->Union[str, None]:
+            return f"Item {item} não existe na lista"
+
+    def insert_item(self, item: int) -> Union[str, None]:
 
         if self.__size == (self.__pointer+1):
             return f"Capacidade máxima atingida"
 
-        if not isinstance(item,int):
+        if not isinstance(item, int):
             return f"Erro. O tipo do item deve ser int"
-        
+
         else:
             self.__vector.append(item)
-            self.__pointer+=1
+            self.__pointer += 1
 
-    def remove_item(self, item:int)->Union[str,None]:
+    def remove_item(self, item: int) -> Union[str, None]:
 
-        if not isinstance(item,int):
+        if not isinstance(item, int):
             return f"Erro. O tipo do item deve ser int"
 
         if self.__pointer == -1:
@@ -56,10 +59,8 @@ class VectorUnsorted:
 
                     while ind < self.length()-1:
                         self.__vector[ind] = self.__vector[ind+1]
-                        ind+= 1
+                        ind += 1
 
                     return f"Item {item} remvido com sucesso"
 
             return f"Item {item} não existe na lista"
-                
-
